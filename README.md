7cf72f03406a8f7b15bac739a93ff897e34bb561
adelefaell <adelefaell@gmail.com>
2026-07-24 21:21:27 +0300

    Updates

---
+++ README.md
@@ -0,0 +1,10 @@
+# Layout-Animations-gsap-flip
+html JavaScript CSS with gasp animations
+
+### NOT responsive!!!!
+
+## Live URL: [Layout Animations gsap flip](https://layout-animations-gsap-flip.github.io/)
+
+
+Preview
+![Preview](./preview.png)
---
+++ main.js
@@ -0,0 +1,53 @@
+gsap.registerPlugin(Flip)
+
+const links = document.querySelectorAll('.nav-item a')
+const activeNav = document.querySelector('.active-nav')
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    gsap.to(links, { color: '#252525' })
+    // turn navs blue
+    if (document.activeElement === link) {
+      gsap.to(link, { color: '#385ae0' })
+    }
+
+    // wanna move the line
+    const state = Flip.getState(activeNav)
+    link.appendChild(activeNav)
+    Flip.from(state, {
+      duration: 1.25,
+      absolute: true,
+      ease: 'elastic.out(1,0.6)',
+    })
+  })
+})
+
+//Cards
+const cards = document.querySelectorAll('.card')
+
+cards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+    // get state
+    const state = Flip.getState(cards)
+
+    //Add the active class to the clicked one and add in-active to the others
+    const isCardActive = card.classList.contains('active')
+
+    cards.forEach((otherCard, otherIdx) => {
+      otherCard.classList.remove('active')
+      otherCard.classList.remove('is-inactive')
+
+      if (!isCardActive && index !== otherIdx) {
+        otherCard.classList.add('is-inactive')
+      }
+    })
+
+    if (!isCardActive) card.classList.add('active')
+
+    Flip.from(state, {
+      duration: 1,
+      ease: 'expo.out',
+      absolute: true,
+    })
+  })
+})
